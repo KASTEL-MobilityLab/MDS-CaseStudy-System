7bf1cb53793c9c416f33c9e26dc4c2ee1989d90e
Debora <debora@pop-os.localdomain>
2022-07-27 11:55:29 +0200

    working script for parsing fake data from json file and posting it to the mds-agency

---
+++ mds-client/index.ts
@@ -0,0 +1,52 @@
+
+import parser from "stream-json";
+import pick from "stream-json/filters/Pick.js";
+import chain from "stream-chain";
+import streamArray from "stream-json/streamers/StreamArray.js";
+import * as fs from "fs";
+
+import axios from "axios";
+
+var bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1N2YxZTA2Mi04NzY2LTRkMDMtYjhjYy00ZTMyYmFmOWRmNmIiLCJodHRwczovL29wZW5tb2JpbGl0eWZvdW5kYXRpb24ub3JnL3VzZXJfZW1haWwiOiJ1c2VyQHRlc3QuYWkiLCJodHRwczovL29wZW5tb2JpbGl0eWZvdW5kYXRpb24ub3JnL3Byb3ZpZGVyX2lkIjoiYmRmODk2YWYtNjQyZC00NTk1LTg0MGItOTMwYzJhZGExZjYzIiwic2NvcGUiOiJhZG1pbjphbGwiLCJpYXQiOjE2NTc4MTEyMDV9.VW82ym6yOzGwWb6xoGiy3QD2NTd1ypvraSoMSaUNfdg";
+var bearer = 'Bearer ' + bearer_token;
+
+const instance = axios.create({
+    baseURL: 'http://172.23.0.22:4000/',
+    headers: {'Authorization': bearer}
+    });
+
+const pipeline = new chain([
+  fs.createReadStream('provider_BSE_status_changes_20220708T050000Z_20220708T210000Z.json'),
+  parser(),
+  new pick({filter: 'data.status_changes'}),
+  new streamArray(),
+  data => {
+    const value = data.value;
+    const postdata = {
+      device_id: value.device_id, vehicle_id: value.vehicle_id, vehicle_type: value.vehicle_type, propulsion_types: value.propulsion_type
+    };
+    
+    instance.post('/vehicles', postdata, {
+        headers: {
+              'Content-Type': 'application/json'
+          },
+        }).then(function (response) {
+          console.log(response.status)
+        }).catch(function (error) {
+          console.log(error)
+        });
+      
+  }
+]);
+
+
+
+
+
+
+
+  
+
+
+
+
