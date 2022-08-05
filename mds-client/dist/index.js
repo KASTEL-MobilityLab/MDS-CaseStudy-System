173ee0793d42e9432cb37050ddeb200e0c527c9c
Debora <debora@pop-os.localdomain>
2022-08-05 10:20:39 +0200

    Removed debugging print lines and await get request so that no race conditions happen

--- mds-client/dist/index.js
+++ mds-client/dist/index.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import parser from "stream-json";
 import pick from "stream-json/filters/Pick.js";
 import chain from "stream-chain";
@@ -5,7 +14,7 @@ import streamArray from "stream-json/streamers/StreamArray.js";
 import * as fs from "fs";
 import axios from "axios";
 import axiosRetry from "axios-retry";
-//JWT bearer_token has been generated out of provider_id which is just a normal uuid and is used to identify the provider
+//JWT bearer_token has been generated out of provider_id which is just a random uuid and is used to identify the provider
 var bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1N2YxZTA2Mi04NzY2LTRkMDMtYjhjYy00ZTMyYmFmOWRmNmIiLCJodHRwczovL29wZW5tb2JpbGl0eWZvdW5kYXRpb24ub3JnL3VzZXJfZW1haWwiOiJ1c2VyQHRlc3QuYWkiLCJodHRwczovL29wZW5tb2JpbGl0eWZvdW5kYXRpb24ub3JnL3Byb3ZpZGVyX2lkIjoiYmRmODk2YWYtNjQyZC00NTk1LTg0MGItOTMwYzJhZGExZjYzIiwic2NvcGUiOiJhZG1pbjphbGwiLCJpYXQiOjE2NTc4MTEyMDV9.VW82ym6yOzGwWb6xoGiy3QD2NTd1ypvraSoMSaUNfdg";
 var bearer = 'Bearer ' + bearer_token;
 //mds-agency is the container name where the api requests go to, works only in the same network (docker compose solves that)
@@ -14,35 +23,31 @@ const instance = axios.create({
     headers: { 'Authorization': bearer }
 });
 axiosRetry(instance, { retries: 15 });
-console.log("Instanz für API Requests erstellt");
 function parseJsonPostRequest() {
-    console.log("Funktion startet die Ausführung");
     const pipeline = new chain([
         fs.createReadStream('provider_status_changes.json'),
         parser(),
         new pick({ filter: 'data.status_changes' }),
         new streamArray(),
-        data => {
+        (data) => __awaiter(this, void 0, void 0, function* () {
             const value = data.value;
             const postdata = {
                 device_id: value.device_id, vehicle_id: value.vehicle_id, vehicle_type: value.vehicle_type, propulsion_types: value.propulsion_type
             };
             var url_device_id = '/vehicles/' + value.device_id;
-            instance.get(url_device_id, {
+            yield instance.get(url_device_id, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
             }).then(function (response) {
-                console.log("Get request done");
                 console.log(response.status);
                 console.log(response.statusText);
             }).catch(function (error) {
-                if (error.response.status == 404) {
-                    console.log("Post Data");
+                if (error.response.status == 404) { //means, no vehicle found for that device_id
                     postData(postdata);
                 }
             });
-        }
+        })
     ]);
 }
 function postData(postdata) {
@@ -51,7 +56,6 @@ function postData(postdata) {
             'Content-Type': 'application/json'
         },
     }).then(function (response) {
-        console.log("Post request done, device added");
         console.log(response.status);
         console.log(response.statusText); //Created
     }).catch(function (error) {
@@ -61,7 +65,5 @@ function postData(postdata) {
         console.log(error.response.data.error);
     });
 }
-//retry implementieren: heißt das, ich mache es 3 Mal oder so und schaue, was für einen Status Code ich zurück bekomme; als Abbruchbedingung 201?
-// bevor ich poste, kann ich ja einen get Request mit der device_id machen und schauen, was passiert, also ob 200 (?) zurück kommt, dann kein post request, oder?
-//setTimeout(parseJsonPostRequest, 60000);
+//setTimeout(parseJsonPostRequest, 60000); //only for debugging reasons to be able to read the log
 parseJsonPostRequest();
--- mds-client/index.ts
+++ mds-client/index.ts
@@ -7,7 +7,7 @@ import * as fs from "fs";
 import axios from "axios";
 import axiosRetry from "axios-retry";
 
-//JWT bearer_token has been generated out of provider_id which is just a normal uuid and is used to identify the provider
+//JWT bearer_token has been generated out of provider_id which is just a random uuid and is used to identify the provider
 var bearer_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1N2YxZTA2Mi04NzY2LTRkMDMtYjhjYy00ZTMyYmFmOWRmNmIiLCJodHRwczovL29wZW5tb2JpbGl0eWZvdW5kYXRpb24ub3JnL3VzZXJfZW1haWwiOiJ1c2VyQHRlc3QuYWkiLCJodHRwczovL29wZW5tb2JpbGl0eWZvdW5kYXRpb24ub3JnL3Byb3ZpZGVyX2lkIjoiYmRmODk2YWYtNjQyZC00NTk1LTg0MGItOTMwYzJhZGExZjYzIiwic2NvcGUiOiJhZG1pbjphbGwiLCJpYXQiOjE2NTc4MTEyMDV9.VW82ym6yOzGwWb6xoGiy3QD2NTd1ypvraSoMSaUNfdg";
 var bearer = 'Bearer ' + bearer_token;
 
@@ -17,35 +17,30 @@ const instance = axios.create({
     headers: {'Authorization': bearer}
     });
 
-axiosRetry(instance, { retries: 15 });    
-
-console.log("Instanz für API Requests erstellt");
+axiosRetry(instance, { retries: 15 });
 
 
 function parseJsonPostRequest() {
-  console.log("Funktion startet die Ausführung");
   const pipeline = new chain([
     fs.createReadStream('provider_status_changes.json'),
     parser(),
     new pick({filter: 'data.status_changes'}),
     new streamArray(),
-    data => {
+    async data => {
       const value = data.value;
       const postdata = {
         device_id: value.device_id, vehicle_id: value.vehicle_id, vehicle_type: value.vehicle_type, propulsion_types: value.propulsion_type
       };
       var url_device_id = '/vehicles/' + value.device_id
-      instance.get(url_device_id, {
+      await instance.get(url_device_id, {
           headers: {
                 'Content-Type': 'application/json'
             },
           }).then(function (response) {
-            console.log("Get request done");
             console.log(response.status);
             console.log(response.statusText);
           }).catch(function (error) {
-            if (error.response.status == 404){
-              console.log("Post Data");
+            if (error.response.status == 404){ //means, no vehicle found for that device_id
               postData(postdata);
             }
             
@@ -63,7 +58,6 @@ function postData(postdata:any) {
         'Content-Type': 'application/json'
       },
     }).then(function (response) {
-      console.log("Post request done, device added")
       console.log(response.status)
       console.log(response.statusText) //Created
     }).catch(function (error) { 
@@ -74,14 +68,7 @@ function postData(postdata:any) {
     });    
 }
 
-//setTimeout(parseJsonPostRequest, 60000);
+//setTimeout(parseJsonPostRequest, 60000); //only for debugging reasons to be able to read the log
 parseJsonPostRequest();
-
-
-
-
-  
-
-
 
 
